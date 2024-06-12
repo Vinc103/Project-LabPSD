5ed2f377fb7f70063d9a5f8c5bb110253a66c003
Vinc102 <vincentvinct.vv10@gmail.com>
2024-06-12 22:46:34 +0700

    Add Detail Product C#

--- Project PSD/View/Detail Product.aspx.cs
+++ Project PSD/View/Detail Product.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_PSD.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,77 @@ namespace Project_PSD.View
 {
     public partial class Detail_Product : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+            EcommerceDbEntities db = new EcommerceDbEntities();
+            protected void Page_Load(object sender, EventArgs e)
+            {
+                if (!IsPostBack)
+                {
+                    int makeupId;
+                    if (int.TryParse(Request.QueryString["MakeupId"], out makeupId))
+                    {
+                        LoadProductDetails(makeupId);
+                    }
+                    else
+                    {
+                        // Handle error if MakeupId is not provided or invalid
+                        Response.Redirect("Home.aspx");
+                    }
+                }
+            }
 
+            private void LoadProductDetails(int makeupId)
+            {
+                var product = db.Makeups.FirstOrDefault(p => p.MakeupId == makeupId);
+                if (product != null)
+                {
+                    ProductName.InnerText = product.ProductName;
+                    ProductPrice.InnerText = $"Rp. {product.Price:N0}";
+                    ProductDescription.InnerText = product.Description;
+                    mainImage.Src = $"../Web Img/{product.ImagePath}";
+                }
+                else
+                {
+                    // Handle error if product not found
+                    Response.Redirect("Home.aspx");
+                }
+            }
+
+            protected void AddToCartBtn_Click(object sender, EventArgs e)
+            {
+                int userId = GetUserId(); // Implement a method to get the logged-in user's ID
+                int makeupId = int.Parse(Request.QueryString["MakeupId"]);
+                int quantity = int.Parse(QuantityTxt.Text);
+
+                var cartItem = db.Carts.FirstOrDefault(c => c.UserId == userId && c.MakeupId == makeupId);
+                if (cartItem != null)
+                {
+                    cartItem.Quantity += quantity;
+                }
+                else
+                {
+                    cartItem = new Cart
+                    {
+                        UserId = userId,
+                        MakeupId = makeupId,
+                        Quantity = quantity
+                    };
+                    db.Carts.Add(cartItem);
+                }
+
+                db.SaveChanges();
+                Response.Redirect("Cart.aspx"); // Redirect to the cart page
+            }
+
+            protected void BuyNowBtn_Click(object sender, EventArgs e)
+            {
+                AddToCartBtn_Click(sender, e); // Add item to cart
+                Response.Redirect("Checkout.aspx"); // Redirect to checkout
+            }
+
+            private int GetUserId()
+            {
+                // Dummy method for getting the user ID. Replace with your actual logic.
+                return 1;
+            }
         }
     }
-}
